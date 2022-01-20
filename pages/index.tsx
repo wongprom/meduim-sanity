e4acfda382bb00f2bd39b96394f8b4124b83151a
wongprom <>
2022-01-20 11:39:40 +0100

    Add baner

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,32 @@ import Header from '../components/Header';
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="max-w-7xl mx-auto">
       <Head>
         <title>Medium blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+      <div className="flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-0">
+        <div className="px-10 space-x-5">
+          <h1 className="text-6xl max-w-xl font-serif">
+            <span className="underline decoration-black decoration-4">
+              Medium
+            </span>{' '}
+            is the place to write, read and connect
+          </h1>
+          <h2>
+            It's easy and free to post your thinking on any topic and connect
+            with millions of readers.
+          </h2>
+        </div>
+        <img
+          className="hidden md:inline-flex h-32 lg:h-full"
+          src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png"
+          alt=""
+        />
+      </div>
+      {/* Posts */}
     </div>
   );
 }
